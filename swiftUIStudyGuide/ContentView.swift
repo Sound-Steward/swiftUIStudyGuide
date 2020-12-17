597781a44d184877249a448dbad58bb763827a26
Kyle Sowards <soundsteward@protonmail.com>
2020-12-16 20:28:44 -0600

    Initial Commit

---
+++ swiftUIStudyGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIStudyGuide
+//
+//  Created by K.J.S.G on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIStudyGuide/swiftUIStudyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIStudyGuideApp.swift
+//  swiftUIStudyGuide
+//
+//  Created by K.J.S.G on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIStudyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
